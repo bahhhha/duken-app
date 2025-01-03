5d4cab715de40b857973571ce64dcd5b2e65bcc3
bahhhha <zharylkassyn001@gmail.com>
2025-01-04 00:53:57 +0500

    minor fixes

--- src/entities/product/no-stock/ui/no-stock.tsx
+++ src/entities/product/no-stock/ui/no-stock.tsx
@@ -4,7 +4,7 @@ export const NoStock = () => {
   return (
     <div className="w-full flex items-center justify-center gap-2 py-2 px-4 bg-zinc-100 rounded-md text-zinc-500">
       <Frown size={16} strokeWidth={2.2} />
-      <span className="text-sm font-medium">Нет в наличии</span>
+      <span className="text-xs font-medium">Нет в наличии</span>
     </div>
   );
 };
